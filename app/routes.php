42d9f9d89899eff984565f95c323c193681f6864
Michaël Hoste <michael.hoste@gmail.com>
2014-08-01 21:17:29 +0200

    syntax of blade interpolation

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+// http://scotch.io/bar-talk/quick-tip-using-laravel-blade-with-angularjs
+Blade::setContentTags('<%', '%>');          // for variables and all things Blade
+Blade::setEscapedContentTags('<%%', '%%>'); // for escaped data
+
 Route::get('/', function() {
 	return View::make('hello');
 });
