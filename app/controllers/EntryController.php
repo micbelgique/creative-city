6040c5b7079bd226d630109e74f80af0f9d22aed
Aurélien Malisart <aurelien.malisart@gmail.com>
2014-08-01 15:20:36 +0200

    Start the create entry page

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -8,11 +8,18 @@ class EntryController extends BaseController {
   }
 
   public function create() {
-    //$entry = Entry::new();
+    $entry = new Entry;
+    return View::make('entries.create')->with('entry', $entry);
   }
 
   public function store() {
+    $entry = new Entry(Input::all());
 
+    if($entry->save()) {
+      return Redirect::route('entries.index')->with('flash', 'Votre post a été envoyé');
+    }
+
+    return Redirect::route('entries.create')->withInput()->withErrors($s->errors());
   }
 
 }
--- app/models/Entry.php
+++ app/models/Entry.php
@@ -5,7 +5,7 @@ use Rhumsaa\Uuid\Uuid;
 class Entry extends Eloquent {
 
   protected $table = 'entries';
-
+  protected $fillable = array('title', 'description', 'url', 'author_name', 'author_email');
 }
 
 Entry::creating(function($entry){
