c756cf59c2cfc5e3f3c7dfd4a4764bd6c988cc17
Michaël Hoste <michael.hoste@gmail.com>
2014-08-01 15:56:57 +0200

    angular and coffeescript

--- public/index.php
+++ public/index.php
@@ -34,7 +34,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/start.php';
 
-SassCompiler::run("public/scss/", "public/compiled-css/");
+SassCompiler::run("app/assets/scss/", "public/compiled-css/");
 
 /*
 |--------------------------------------------------------------------------
