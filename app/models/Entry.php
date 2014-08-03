1b5919718d8b393e3b339b19a9e89835f6fad258
Francois Stephany <tulipe.moutarde@gmail.com>
2014-08-03 14:02:07 +0200

    Brol

--- app/models/Entry.php
+++ app/models/Entry.php
@@ -16,8 +16,8 @@ class Entry extends Eloquent implements StaplerableInterface {
   public function __construct(array $attributes = array()) {
     $this->hasAttachedFile('picture', [
         'styles' => [
-          'large'  => '1140x550',
-          'medium' => '360x210',
+          'large'  => '1140x550#',
+          'medium' => '360x210#',
           'thumb'  => '100x100'
         ]
     ]);
